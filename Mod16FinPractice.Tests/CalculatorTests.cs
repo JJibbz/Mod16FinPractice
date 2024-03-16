66796dd2122ccaafd530cf4ec6f9c604beb1df64
JJibbz <vkazancev322@gmail.com>
2024-03-16 21:18:03 +0700

    Добавьте файлы проекта.

---
+++ Mod16FinPractice.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Mod16FinPractice.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(2, 2) == 4);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(4, 2) == 2);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue() 
+        {  
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Multiplication(4, 2) == 8);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue() 
+        {  
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(4, 2) == 2);
+        }
+    }
+}
---
+++ Mod16FinPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mod16FinPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
